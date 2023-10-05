30a9875df0ce7a104214f7d8c019253cf9ad8d6e
15Carmen <carmen.martin@iesnervion.es>
2023-10-05 14:41:10 +0200

    no se que hacer en el 2

---
+++ Tema5/Tema5_Ejercicio2_Controles/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tema5_Ejercicio2_Controles
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
