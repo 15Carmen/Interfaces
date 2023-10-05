cbe7a43723282ec43709efeb4418c8b29283c5f5
15Carmen <carmen.martin@iesnervion.es>
2023-10-05 14:06:13 +0200

    lol

---
+++ Tema5/Tema5_Ejercicio1_HelloWorld/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tema5_Ejercicio1_HelloWorld
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
