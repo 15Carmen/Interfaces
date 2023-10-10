532e19c19ce8865a1c4c221c6958c3b1624f94c3
15Carmen <carmen.martin@iesnervion.es>
2023-10-10 14:43:22 +0200

    c# es dificil

---
+++ Tema6/Tema6/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema6
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tema6/Tema6/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Tema6
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickedBtn2(object sender, EventArgs e)
+        {
+            btn3.IsVisible = true;
+        }
+
+        private void ClickedBtn3(object sender, EventArgs e)
+        {
+            btn1.IsVisible = false;
+            btn2.Text = "Crear controles en tiempo de ejecución mola";
+        }
+
+
+    }
+}
