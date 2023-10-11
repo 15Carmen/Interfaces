9d4757c09a0e5ae4053d1b3235bba72577ac57db
15Carmen <carmen.martin@iesnervion.es>
2023-10-11 12:45:17 +0200

    lol

--- Tema6/Tema6/MainPage.xaml.cs
+++ Tema6/Tema6/MainPage.xaml.cs
@@ -11,7 +11,16 @@
 
         private void ClickedBtn2(object sender, EventArgs e)
         {
-            btn3.IsVisible = true;
+            //btn3.IsVisible = true;
+            
+            Button btn3 = new Button();
+            btn3.Text = "Boton3";
+            btn3.HorizontalOptions = LayoutOptions.Center;
+            btn3.VerticalOptions = LayoutOptions.Center;
+            btn3.BackgroundColor = Colors.CornflowerBlue;
+
+
+
         }
 
         private void ClickedBtn3(object sender, EventArgs e)
@@ -19,6 +28,8 @@
             btn1.IsVisible = false;
             btn2.Text = "Crear controles en tiempo de ejecución mola";
         }
+
+       
 
 
     }
