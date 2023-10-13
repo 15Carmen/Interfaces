c61f5ad9da270b0378d243fe8caccfc67957a67e
15Carmen <carmen.martin@iesnervion.es>
2023-10-13 09:36:36 +0200

    ejercicio tema 6 hecho

--- Tema6/Tema6/MainPage.xaml.cs
+++ Tema6/Tema6/MainPage.xaml.cs
@@ -9,24 +9,41 @@
             InitializeComponent();
         }
 
+        Boolean btnYaCreado = false;
+
         private void ClickedBtn2(object sender, EventArgs e)
         {
             //btn3.IsVisible = true;
-            
-            Button btn3 = new Button();
-            btn3.Text = "Boton3";
-            btn3.HorizontalOptions = LayoutOptions.Center;
-            btn3.VerticalOptions = LayoutOptions.Center;
-            btn3.BackgroundColor = Colors.CornflowerBlue;
 
+            if (btnYaCreado!=true)
+            {
+                Button btn3 = new Button();
+                
+                btn3.Text = "Boton3";
+                btn3.HorizontalOptions = LayoutOptions.Center;
+                btn3.VerticalOptions = LayoutOptions.Center;
+                btn3.BackgroundColor = Colors.CornflowerBlue;
+                btn3.HeightRequest = 70;
+                btn3.WidthRequest = 200;
+                btn3.FontFamily = "Verdana";
+                btn3.FontSize = 16;
+                btn3.BorderColor = Colors.Yellow;
+                btn3.Margin = 30;
 
+                botones.Add(btn3);
+                btn3.Clicked += ClickedBtn3;
+                btnYaCreado = true;
 
+            }
+
+           
         }
 
         private void ClickedBtn3(object sender, EventArgs e)
         {
-            btn1.IsVisible = false;
+            botones.Remove(btn1);
             btn2.Text = "Crear controles en tiempo de ejecución mola";
+            btn2.WidthRequest = 400;
         }
 
        
