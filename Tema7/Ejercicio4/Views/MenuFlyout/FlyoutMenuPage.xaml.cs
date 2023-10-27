5581c2ca8a1dee6c0c07fabecb1588ba32d58d2f
15Carmen <carmen.martin@iesnervion.es>
2023-10-27 11:47:59 +0200

    bueno pues a empezar tema nuevo

---
+++ Tema7/Ejercicio4/Views/MenuFlyout/FlyoutMenuPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio4.Views.MenuFlyout;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Tema8/Ejercicio1_StackLayout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio1_StackLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
