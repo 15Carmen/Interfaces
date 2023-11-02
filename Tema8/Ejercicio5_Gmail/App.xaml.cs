c7e5d54b86b992ede7e36670da1de2f0e9543701
15Carmen <carmen.martin@iesnervion.es>
2023-11-02 13:53:19 +0100

    dios mio que he hecho para merecer este martirio

---
+++ Tema8/Ejercicio5_Gmail/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio5_Gmail
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
