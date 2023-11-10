4212f8129361aa319f7db554c6dfba0b60d3fa51
15Carmen <carmen.martin@iesnervion.es>
2023-11-10 10:18:15 +0100

    bueno el diseño es bonito por lo menos

--- CarmenMartin_Examen9Nov/CarmenMartin_Examen9Nov/MainPage.xaml.cs
+++ CarmenMartin_Examen9Nov/CarmenMartin_Examen9Nov/MainPage.xaml.cs
@@ -20,31 +20,36 @@ namespace CarmenMartin_Examen9Nov
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private /*async*/ void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        private async void TapImagen(object sender, TappedEventArgs e)
         {
             contadorErrores++;
 
             if(contadorErrores > 3)
             {
 
-                DisplayAlert("¡Has perdido!", "¿Quieres volver a Jugar?", "Si", "No");
+                bool volverAJugar = await DisplayAlert("¡Has perdido!", "¿Quieres volver a Jugar?", "Si", "No");
 
                 //Si el usuario elige la opción de volver a jugar, reestablecemos los contadores
-                /*if ("Si")
+                if (volverAJugar)
                 {
                     contadorErrores = 0;
                     contadorDiferencias = 0;
-                    await Navigation.PushAsync(new MainPage()); 
+                    
 
                 }else{
                 
-                    //LA aplicacaión se cierra
-                 }*/
+                    Application.Current.Quit();
+                 }
             }
         }
 
         //Falta una función que cuando se clique la zona donde haya una diferencia, se haga visible 
         //la elipse que la marca y qeu añada 1 punto al contador de difererencias
+
+        private void TapEllipse(object sender, TappedEventArgs e)
+        {
+
+        }
 
 
        
