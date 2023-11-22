d742b74b5609414cc3d8ddbb48ea187ee1425eb9
15Carmen <carmen.martin@iesnervion.es>
2023-11-22 14:41:26 +0100

    me meo

---
+++ Tema10/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+
+            
+
+             
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ Tema10/Tema10/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tema10.Models;
+
+
+namespace Tema10.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
