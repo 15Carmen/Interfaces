14375e5be066a8a6fb1d1aaa7e18665518922738
15Carmen <carmen.martin@iesnervion.es>
2023-11-30 14:37:21 +0100

    comandos

---
+++ Tema10/Ejercicio3_Calculadora/ViewModels/MainPageVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ejercicio3_Calculadora.ViewModels
+{
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region atributos
+
+        private string labelShowOperation = "0";
+        #endregion
+
+        #region propiedades
+
+        public string LabelShowOperation
+        {
+            get { return labelShowOperation; }
+        }
+
+        public ICommand comandoLimpiar { get; set; }
+        public ICommand comandoBorrarCifra {  get; set; }
+        public ICommand comandoOperaciones { get; set; }
+        public ICommand comandoNumeros { get; set; }
+        public ICommand comandoResultado { get; set; }
+
+        #endregion
+
+        #region constructores
+
+        public MainPageVM()
+        {
+            
+           
+        }
+
+        #endregion
+
+
+
+    }
+}
