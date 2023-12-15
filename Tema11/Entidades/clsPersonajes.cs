82e99fcf8b2eba40ef78c14e68feb3509ac38255
15Carmen <carmen.martin@iesnervion.es>
2023-12-15 12:11:19 +0100

    lol

---
+++ Tema11/Entidades/clsPersonajes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clsPersonajes
+    {
+
+        #region
+
+        private string name;
+        private int height;
+        private int mass;
+        private string hair_color;
+        private string skin_color;
+        private string eye_color;
+        private string birth_year;
+        private string gender;
+        private string homeworld;
+       
+        #endregion
+
+    }
+}
