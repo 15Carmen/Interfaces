3bd976a2ea9252a3830041c1cb3d199321ef2823
15Carmen <carmen.martin@iesnervion.es>
2024-01-19 11:57:45 +0100

    jajant

---
+++ Tema12/Maui/ViewModels/MainPageVM.cs
@@ -0,0 +1,74 @@
+﻿using Ejercicio1.ViewModels.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.Collections.ObjectModel;
+
+namespace Maui.ViewModels
+{
+    public class MainPageVM : clsVMBase
+    {
+
+        #region atributos
+
+        private clsMensajeUsuario oMensajeUsuario;
+        private string nombreUsuario;
+        private string mensajeUsuario;
+        private DelegateCommand btnEnviarMensaje;
+        private ObservableCollection<clsMensajeUsuario> listaChats;
+
+        #endregion
+
+        #region constructores
+
+        public MainPageVM()
+        {
+            listaChats = new ObservableCollection<clsMensajeUsuario>();
+            btnEnviarMensaje = new DelegateCommand(enviarExecute, enviarCanExecute);
+        }
+
+        #endregion
+
+        #region propiedades
+
+        public string NombreUsuario{
+            get { return nombreUsuario; }
+            set {  nombreUsuario = value; }
+        }
+
+        public string MensajeUsuario
+        {
+            get { return mensajeUsuario; }
+            set { mensajeUsuario = value; }
+        }
+
+        #endregion
+
+        #region commands
+
+        public void enviarExecute()
+        {
+            //TODO: comando enviar mensaje
+        }
+
+        public bool enviarCanExecute()
+        {
+            bool enviar = false;
+
+            if(!string.IsNullOrEmpty(NombreUsuario) || !string.IsNullOrEmpty(MensajeUsuario))
+            {
+                enviar = true;
+            }
+
+            return enviar;
+        }
+
+
+
+        #endregion
+
+    }
+}
